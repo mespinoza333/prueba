08f67b17a6ca3dab9e42cba2663384ab1fe640e0
ME <mespinoza1@protonmail.com>
2025-07-25 17:32:15 -0500

    csat.ps1

---
+++ csat.ps1
@@ -0,0 +1,1 @@
+powershell.exe -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/mespinoza333/Scripts/refs/heads/main/crear_carpeta.ps1' -OutFile 'C:\Windows\Temp\crear_carpeta.ps1'"
